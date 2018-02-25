40e9b31f42e168bd5f871d8fbca4e5d5d1f65231
akuprienko <a.kouprienko@corvusalba.ru>
2018-02-26 01:20:35 +0300

    Кое-что забыл добавить

--- ParentsSite/SantexFrontend/src/app/core/home/return-purchases/return-purchases.module.ts
+++ ParentsSite/SantexFrontend/src/app/core/home/return-purchases/return-purchases.module.ts
@@ -8,7 +8,7 @@ import { ReturnPurchaseComponent } from './return-purchase/return-purchase.compo
 import { HomeSharedModule } from '../shared/home-shared.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ReturnPurchasesRoutingModule } from './return-purchases-routing.module';
-import { ReturnPurchasesReducer } from './store/return-purchases.reducers';
+import { returnPurchasesReducer } from './store/return-purchases.reducers';
 
 import { MyDatePickerModule } from 'angular4-datepicker/src/my-date-picker/my-date-picker.module';
 
---
+++ ParentsSite/SantexFrontend/src/app/core/home/return-purchases/src/ReturnReason.ts
@@ -0,0 +1,4 @@
+export enum ReturnReason{
+    Defect = 0,
+    Other = 1
+}
